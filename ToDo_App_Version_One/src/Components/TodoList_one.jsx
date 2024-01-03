c587e945b0cc8340a07cff01a6e283dc4adbfd7f
Abhijit Das <abhijitd721201@gmail.com>
2024-01-03 12:00:01 +0530

    Dinamic change on TodoList_one

--- ToDo_App_Version_One/src/Components/TodoList_one.jsx
+++ ToDo_App_Version_One/src/Components/TodoList_one.jsx
@@ -1,9 +1,11 @@
 function Todo1() {
+  let todoName = "Buy Milk";
+  let todoDate = "03/01/2024";
   return (
     <div class="container text-center">
       <div class="row">
-        <div class="col-4">Buy Milk</div>
-        <div class="col-4">03/01/2024</div>
+        <div class="col-4">{todoName}</div>
+        <div class="col-4">{todoDate}</div>
         <div class="col-4">
           <button type="button" class="btn btn-danger">
             Delete
