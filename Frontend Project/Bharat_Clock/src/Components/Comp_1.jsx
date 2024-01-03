37ac712f50979b502d73e32c891661ce95003f78
Abhijit Das <abhijitd721201@gmail.com>
2024-01-03 15:28:37 +0530

    Bharat Clock using React

---
+++ Frontend Project/Bharat_Clock/src/Components/Comp_1.jsx
@@ -0,0 +1,9 @@
+function Clock_Heading() {
+  return (
+    <center>
+      <h1>Bharat Clock</h1>
+    </center>
+  );
+}
+
+export default Clock_Heading;
---
+++ Frontend Project/Bharat_Clock/src/Components/Comp_3.jsx
@@ -0,0 +1,11 @@
+let CurrentTime = () => {
+  return (
+    <center>
+      <div>
+        <p>This is the Current time : </p>
+      </div>
+    </center>
+  );
+};
+
+export default CurrentTime;
