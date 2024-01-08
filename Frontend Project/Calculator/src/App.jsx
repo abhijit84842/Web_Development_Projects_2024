84436e428a14fd0e9984d1f341ebe95bd8d13bd6
Abhijit Das <abhijitd721201@gmail.com>
2024-01-08 16:57:07 +0530

    first commit

---
+++ Frontend Project/Calculator/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <center>
+      <div id="calculatoe">
+        <input id="display" type="text"></input>
+        <div id="button-container">
+          <button>C</button>
+        </div>
+      </div>
+    </center>
+  );
+}
+
+export default App;
