9563f04f513164b2d0866a5ee86633d41e5ffed8
Abhijit Das <abhijitd721201@gmail.com>
2024-01-12 16:44:03 +0530

    first commit

---
+++ Frontend Project/ToDO App/ToDo_App_Version_3/src/Components/AddTodo.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { MdOutlineAddCircleOutline } from "react-icons/md";
+
+function AddTodo({ onNewItem }) {
+  const [todoName, setTodoName] = useState("");
+  const [dueDate, setDueDate] = useState("");
+
+  const handleNameChange = (event) => {
+    setTodoName(event.target.value);
+  };
+
+  const handleDateChange = (event) => {
+    setDueDate(event.target.value);
+  };
+
+  const handleAddButtonClicked = () => {
+    onNewItem(todoName, dueDate);
+
+    // blank the current value
+    setTodoName("");
+    setDueDate("");
+  };
+  return (
+    <div className="container">
+      <div className="row kgRow">
+        <div className="col-4">
+          <input
+            type="text"
+            placeholder="Enter Todo here.."
+            value={todoName}
+            onChange={handleNameChange}
+          />
+        </div>
+
+        <div className="col-4">
+          <input type="date" value={dueDate} onChange={handleDateChange} />
+        </div>
+
+        <div className="col-4">
+          <button
+            type="button"
+            className="btn btn-success kgButton"
+            onClick={() => handleAddButtonClicked()}
+          >
+            <MdOutlineAddCircleOutline />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddTodo;
