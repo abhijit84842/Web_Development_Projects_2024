1992425b1deb37fa7298491b26a9b806ac5e13ec
Abhijit Das <abhijitd721201@gmail.com>
2024-01-26 15:01:30 +0530

    v-6.0

---
+++ Frontend Project/ToDO App/ToDo_App_Version_6/src/Components/TodoItems.jsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import Todo1 from "./TodoList_one";
+import { TodoItemsContext } from "../store/todo-items-store";
+
+const TodoItems = () => {
+  const contextObj = useContext(TodoItemsContext);
+  const todoItems = contextObj.todoItems;
+  const deleteItem = contextObj.deleteItem;
+  //console.log(todoItems);
+  return (
+    <>
+      {todoItems.map((list) => (
+        <Todo1
+          key={list.name}
+          todoName={list.name}
+          todoDate={list.dueDate}
+          onDeleteClick={deleteItem}
+        />
+      ))}
+    </>
+  );
+};
+
+export default TodoItems;
