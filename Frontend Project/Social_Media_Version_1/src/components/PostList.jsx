5030f6e0823cdac2c1ca5d83e68f97b8eac918bf
Abhijit Das <abhijitd721201@gmail.com>
2024-02-08 11:39:14 +0530

    Handling loading state

--- Frontend Project/Social_Media_Version_1/src/components/PostList.jsx
+++ Frontend Project/Social_Media_Version_1/src/components/PostList.jsx
@@ -1,23 +1,29 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "../components/Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
+  // using for loading Spinner..
+  const [fetching, setFetching] = useState(false);
 
   // useEffect => we pass 2 parameter 1 ) anonymous function  ,
   useEffect(() => {
+    setFetching(true);
     fetch("https://dummyjson.com/posts") // use API
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         addInitialPosts(data.posts);
+        setFetching(false);
       });
   }, []);
 
   return (
     <>
-      {postList.length === 0 && <WelcomeMessage />}
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 && <WelcomeMessage />}
       {postList.map((list) => (
         <Post key={list.id} post={list} />
       ))}
