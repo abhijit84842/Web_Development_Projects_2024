bc819e7493a2af4f82046a116832ea9be0fce235
Abhijit Das <abhijitd721201@gmail.com>
2024-02-15 17:03:49 +0530

    update

--- Frontend Project/Myntra Clone Using React/Myntra Clone/src/components/Header.jsx
+++ Frontend Project/Myntra Clone Using React/Myntra Clone/src/components/Header.jsx
@@ -1,14 +1,19 @@
+import { IoMdPerson } from "react-icons/io";
+import { FaHeart } from "react-icons/fa";
+import { FaCartPlus } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <header>
       <div className="logo_container">
-        <a href="#">
+        <Link to="/">
           <img
             className="myntra_home"
             src="images/myntra_logo.webp"
             alt="Myntra Home"
           />
-        </a>
+        </Link>
       </div>
       <nav className="nav_bar">
         <a href="#">Men</a>
@@ -29,24 +34,21 @@ const Header = () => {
       </div>
       <div className="action_bar">
         <div className="action_container">
-          <span className="material-symbols-outlined action_icon">person</span>
+          <IoMdPerson />
+
           <span className="action_name">Profile</span>
         </div>
 
         <div className="action_container">
-          <span className="material-symbols-outlined action_icon">
-            favorite
-          </span>
+          <FaHeart />
           <span className="action_name">Wishlist</span>
         </div>
 
-        <a className="action_container" href="pages/bag.html">
-          <span className="material-symbols-outlined action_icon">
-            shopping_bag
-          </span>
+        <Link className="action_container" to="/bag">
+          <FaCartPlus />
           <span className="action_name">Bag</span>
           <span className="bag-item-count">0</span>
-        </a>
+        </Link>
       </div>
     </header>
   );
---
+++ Frontend Project/Myntra Clone Using React/Myntra Clone/src/routes/Bag.jsx
@@ -0,0 +1,13 @@
+const Bag = () => {
+  return (
+    <main>
+      <div className="bag-page">
+        <div className="bag-items-container"></div>
+        Abhijit Das
+        <div className="bag-summary"></div>
+      </div>
+    </main>
+  );
+};
+
+export default Bag;
