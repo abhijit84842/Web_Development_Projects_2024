3aa8a8ad5b87446d4275c1aaa6471fdb931c810a
Abhijit Das <abhijitd721201@gmail.com>
2024-02-16 17:38:58 +0530

    update

---
+++ Frontend Project/Myntra Clone Using React/Myntra Clone/src/store/bagSlice.js
@@ -0,0 +1,14 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const bagSlice = createSlice({
+    name: 'bag',
+    initialState: [],
+    reducers: {
+        addTobag: (state , action)=>{
+            state.push(action.payload);
+        }
+    }
+})
+
+export const bagSliceActions =bagSlice.actions;
+export default bagSlice;
--- Frontend Project/Myntra Clone Using React/Myntra Clone/src/store/index.js
+++ Frontend Project/Myntra Clone Using React/Myntra Clone/src/store/index.js
@@ -1,10 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit"
 import itemsSlice from "./itemsSlice";
+import fetchStatusSlice from "./fetchStatusSlice";
+import bagSlice from "./bagSlice";
 
 
 // creating the store to combaine multiple reducer in one store
 const myntraStore = configureStore({reducer : {
-    items: itemsSlice.reducer
+    items: itemsSlice.reducer,
+    fetchStatus: fetchStatusSlice.reducer,
+    bag: bagSlice.reducer,
 }})
 
 export default myntraStore;
