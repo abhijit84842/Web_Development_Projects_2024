34a229946617e2aabaf679ff3a29390bbc4a7fc0
Abhijit Das <abhijitd721201@gmail.com>
2024-02-28 17:28:02 +0530

    create popular component

---
+++ MERN Stack Project/Wear Collection E-commerce/Frontend/src/Components/Item/PopularItems.jsx
@@ -0,0 +1,18 @@
+const PopularItems = ({ item }) => {
+  return (
+    <div className="item-container">
+      <img className="item-image" src={item.image} alt="" />
+      <div className="rating">
+        {item.stars} ⭐ | {item.rating.count}
+      </div>
+      <div className="company-name">{item.company}</div>
+      <div className="item-name">{item.item_name}</div>
+      <div className="price">
+        <span className="current-price">Rs {item.current_price}</span>
+        <span className="orginal-price">Rs {item.orginal_price}</span>
+        <span className="discount">({item.discount_percentage} % OFF)</span>
+      </div>
+    </div>
+  );
+};
+export default PopularItems;
--- MERN Stack Project/Wear Collection E-commerce/Frontend/src/Components/Popular/Popular.jsx
+++ MERN Stack Project/Wear Collection E-commerce/Frontend/src/Components/Popular/Popular.jsx
@@ -1,12 +1,15 @@
 import "./Popular.css";
 import data_product from "../Assets/data";
+import PopularItems from "../Item/PopularItems";
 const Popular = () => {
   return (
-    <div className="popular">
-      <h1>POPULAR IN WOMEN</h1>
-      <hr />
-      <div className="popular-item"></div>
-    </div>
+    <main>
+      <div className="populars-container">
+        {data_product.map((item) => (
+          <PopularItems key={item.id} item={item} />
+        ))}
+      </div>
+    </main>
   );
 };
 
