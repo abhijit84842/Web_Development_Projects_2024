7f41d951f9ce0569a4c76861aa6ac4a654c8bf46
Abhijit Das <abhijitd721201@gmail.com>
2024-02-28 13:05:35 +0530

    hero section complete

---
+++ MERN Stack Project/Wear Collection E-commerce/Frontend/src/Components/Hero/Hero.jsx
@@ -0,0 +1,30 @@
+import "./Hero.css";
+import hand_icon from "../Assets/hand_icon.png";
+import arrow_icon from "../Assets/arrow.png";
+import hero_img from "../Assets/hero_image.png";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-left">
+        <h2>NEW ARRIVALS ONLY</h2>
+        <div className="hand-hand-icon">
+          <p>new</p>
+          <img className="child-1" src={hand_icon} alt="" />
+        </div>
+        <p>Collections</p>
+        <p>for everyone</p>
+
+        <div className="hero-latest-btn">
+          <div>Latest Collections </div>
+          <img src={arrow_icon} alt="" />
+        </div>
+      </div>
+
+      <div className="hero-right">
+        <img src={hero_img} alt="" />
+      </div>
+    </div>
+  );
+};
+export default Hero;
--- MERN Stack Project/Wear Collection E-commerce/Frontend/src/routes/App.jsx
+++ MERN Stack Project/Wear Collection E-commerce/Frontend/src/routes/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "../Components/Footer/Footer";
 import Header from "../Components/Header/Header";
+import Hero from "../Components/Hero/Hero";
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
       <Footer />
     </>
   );
