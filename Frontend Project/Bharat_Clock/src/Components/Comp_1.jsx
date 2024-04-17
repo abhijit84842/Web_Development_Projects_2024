033307faede2d74e3bd9b3acea92dd04516a903e
Abhijit Das <95925654+abhijit84842@users.noreply.github.com>
2024-04-17 12:02:57 +0530

    Delete Frontend Project directory
    deleted

--- Frontend Project/Bharat_Clock/src/Components/Comp_1.jsx
+++
@@ -1,9 +0,0 @@
-function Clock_Heading() {
-  return (
-    <center>
-      <h1 className="fw-bolder">Bharat Clock</h1>
-    </center>
-  );
-}
-
-export default Clock_Heading;
--- Frontend Project/Social_Media_Version_1/src/components/WelcomeMessage.jsx
+++
@@ -1,9 +0,0 @@
-const WelcomeMessage = () => {
-  return (
-    <center>
-      <h1 className="welcome-message">There are no post</h1>
-    </center>
-  );
-};
-
-export default WelcomeMessage;
