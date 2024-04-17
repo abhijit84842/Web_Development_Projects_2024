ab6f04bcb70f919552f46747280ba28521471e31
Abhijit Das <abhijitd721201@gmail.com>
2024-04-17 11:50:40 +0530

    create bharat clock

---
+++ Frontend Project(React Js)/Bharat_Clock/src/Components/Comp_1.jsx
@@ -0,0 +1,9 @@
+function Clock_Heading() {
+  return (
+    <center>
+      <h1 className="fw-bolder">Bharat Clock</h1>
+    </center>
+  );
+}
+
+export default Clock_Heading;
---
+++ Frontend Project(React Js)/Bharat_Clock/src/Components/Comp_2.jsx
@@ -0,0 +1,11 @@
+let Clock_Slogan = () => {
+  return (
+    <center>
+      <p className="lead">
+        This the clock that shows the time in Bharat at all times.
+      </p>
+    </center>
+  );
+};
+
+export default Clock_Slogan;
