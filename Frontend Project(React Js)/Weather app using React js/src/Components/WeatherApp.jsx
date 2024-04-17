9090925fb0d02e0b338989016e8106d56f09dbfc
Abhijit Das <abhijitd721201@gmail.com>
2024-04-17 18:36:11 +0530

    fontend look complete

--- Frontend Project(React Js)/Weather app using React js/src/Components/WeatherApp.jsx
+++ Frontend Project(React Js)/Weather app using React js/src/Components/WeatherApp.jsx
@@ -1,5 +1,8 @@
 import "./WeatherApp.css";
 import { GoSearch } from "react-icons/go";
+import cloud_icon from "../assets/cloud.png";
+import humidity from "../assets/humidity.png";
+import wind from "../assets/wind.png";
 
 const WeatherApp = () => {
   return (
@@ -11,7 +14,27 @@ const WeatherApp = () => {
         </div>
       </div>
       <div className="weather-img">
-        <img src="#" alt="" />
+        <img src={cloud_icon} alt="loading..." />
+      </div>
+      <div className="weather-temp">24°C</div>
+      <div className="location">Kolkata</div>
+
+      <div className="data-container">
+        <div className="left-humidity">
+          <div className="humidity">
+            <img src={humidity} alt="" />
+            <span className="percentage">74%</span>
+          </div>
+          <p className="humidity-name">Humidity</p>
+        </div>
+
+        <div className="right-wind">
+          <div className="wind">
+            <img src={wind} alt="" />
+            <span className="wind-speed">5.14 km/h</span>
+          </div>
+          <p className="wind-name">Wind Speed</p>
+        </div>
       </div>
     </div>
   );
