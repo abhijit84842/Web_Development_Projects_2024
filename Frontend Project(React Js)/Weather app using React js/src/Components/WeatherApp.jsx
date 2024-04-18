6de02efa2537f8ca0bdef83be9008cffc90f0a40
Abhijit Das <abhijitd721201@gmail.com>
2024-04-18 15:27:46 +0530

    api testing and fetch data and change innerHtml

--- Frontend Project(React Js)/Weather app using React js/src/Components/WeatherApp.jsx
+++ Frontend Project(React Js)/Weather app using React js/src/Components/WeatherApp.jsx
@@ -5,11 +5,50 @@ import humidity from "../assets/humidity.png";
 import wind from "../assets/wind.png";
 
 const WeatherApp = () => {
+  // API Use...
+  let api_key = "29d264dc25d43fed56ad3fa0f447e26f";
+
+  const search = async () => {
+    const element = document.getElementsByClassName("cityInput");
+    let inputValue = element[0].value;
+    // console.log(inputValue);
+    if (element[0].value === "") {
+      return 0;
+    }
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=Metric&appid=${api_key}`;
+
+    let reponse = await fetch(url);
+    let data = await reponse.json();
+    // console.log(data);
+
+    const temprature = document.getElementsByClassName("weather-temp");
+    const location = document.getElementsByClassName("location");
+    const humidity = document.getElementsByClassName("percentage");
+    const wind = document.getElementsByClassName("wind-speed");
+
+    // update innerHtml
+    temprature[0].innerHTML = data.main.temp + "°C";
+    location[0].innerHTML = data.name;
+    humidity[0].innerHTML = data.main.humidity + "%";
+    wind[0].innerHTML = data.wind.speed + "km/h";
+  };
+
   return (
     <div className="weather-container">
       <div className="input-field">
-        <input type="text" placeholder="Enter your city" required />
-        <div className="search-icon">
+        <input
+          type="text"
+          className="cityInput"
+          placeholder="Enter your city"
+          required
+        />
+        <div
+          className="search-icon"
+          onClick={() => {
+            search();
+          }}
+        >
           <GoSearch size="2rem" />
         </div>
       </div>
