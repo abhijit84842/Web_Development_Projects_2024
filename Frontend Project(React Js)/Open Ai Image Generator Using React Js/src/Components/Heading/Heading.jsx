6e9c095768bc2c9bd1b78c99e2ca0285e9eb38b1
Abhijit Das <abhijitd721201@gmail.com>
2024-04-20 12:37:31 +0530

    update input section

--- Frontend Project(React Js)/Open Ai Image Generator Using React Js/src/Components/Heading/Heading.jsx
+++ Frontend Project(React Js)/Open Ai Image Generator Using React Js/src/Components/Heading/Heading.jsx
@@ -1,6 +1,10 @@
 import "./Heading.css";
 const Heading = () => {
-  return <h1>Ai Image Generator</h1>;
+  return (
+    <h1>
+      Ai Image <span>Generator</span>
+    </h1>
+  );
 };
 
 export default Heading;
