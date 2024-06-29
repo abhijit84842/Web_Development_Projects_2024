4a8fc43441d9ee36adacf3a77a6da67c2966f7f8
Abhijit Das <abhijitd721201@gmail.com>
2024-06-29 12:13:22 +0530

    setup ejs view engine

--- MERN Stack Project/CitySling_E-com_in_NodeJs/app.js
+++ MERN Stack Project/CitySling_E-com_in_NodeJs/app.js
@@ -1,6 +1,19 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
+
+// to read the json data
+app.use(express.json())
+
+// to read the form data
+app.use(express.urlencoded({extended:true}))
+
+// static file
+app.use(express.static(path.join(__dirname , "public")))
+
+// setup view engine for EJS 
+app.set("view engine" , "ejs")
 
 app.get("/", (req,res)=>{
     res.send("server is running..")
