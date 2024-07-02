2374f48d0b722b7d3d7b05105ce107e5e18c0f74
Abhijit Das <abhijitd721201@gmail.com>
2024-07-02 11:41:13 +0530

    create a new owner

--- MERN Stack Project/CitySling_E-com_in_NodeJs/routes/ownersRouter.js
+++ MERN Stack Project/CitySling_E-com_in_NodeJs/routes/ownersRouter.js
@@ -1,5 +1,9 @@
 const express = require("express");
 
+// model require
+const OwnerModel= require("../models/ownerModel");
+const ownerModel = require("../models/ownerModel");
+
 const router = express.Router();
 
 //* * Only Development Base routing in top...* *//
@@ -7,8 +11,22 @@ const router = express.Router();
 // conditional routing..
 if (process.env.NODE_ENV === "development") {
   // this route only show in dev mode not production mode
-  router.post("/create", (req, res) => {
-    res.send("create");
+  router.post("/create", async(req, res) => {
+   let owner= await OwnerModel.find()
+   if(owner.length > 0){
+    return res.send("You don't have permission to create a new owner").status(503)
+   }else{
+    let result=await ownerModel.create({
+      fullname:"Abhijit Das",
+      email:"abhijit721201@gmail.com",
+      password:"123",
+      gstin:"GSTN1203145SGV",
+     
+    })
+    res.send("Owner Created Successfully..").status(200)
+   }
+   
+   
   });
 }
 
