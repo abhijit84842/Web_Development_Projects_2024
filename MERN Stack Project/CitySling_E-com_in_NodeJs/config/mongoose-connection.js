d202d4d92e559dd61ab67ef115dc228f7d3d10da
Abhijit Das <abhijitd721201@gmail.com>
2024-07-02 10:29:53 +0530

    update

--- MERN Stack Project/CitySling_E-com_in_NodeJs/config/mongoose-connection.js
+++ MERN Stack Project/CitySling_E-com_in_NodeJs/config/mongoose-connection.js
@@ -1,31 +1,24 @@
-// Connect with MongoDB 
+// Connect with MongoDB
 
 const { default: mongoose } = require("mongoose");
-const config= require("config") 
+const config = require("config");
 
-// dev mode console using set env variable 
-const dbgr = require("debug")("development:mongoose")
+// dev mode console using set env variable //
+const dbgr = require("debug")("development:mongoose");
 
-
-//// production mode console using set env variable
+//* production mode console using set env variable *//
 // const dbgrp = require("debug")("production:mongoose")
 
-
-
-
-// const DbConnection= async()=>{
-//     try{
-//         await mongoose.connect(config.get("MONGODB_URL"))       // GET connection string by config
-//         dbgr("DB Connected Successfully...")
-//         // dbgrp("DB Connected Successfully...")
-//     }catch(err){
-//         dbgr("not connected ... " + err.message)
-//         // dbgrp("not connected ... " + err.message)
-//     }
-// }  
-
-
-
+const DbConnection = async () => {
+  try {
+    await mongoose.connect(config.get("MONGODB_URL")); // GET connection string by config
+    dbgr("DB Connected Successfully...");
+    // dbgrp("DB Connected Successfully...")
+  } catch (err) {
+    dbgr("not connected ... " + err.message);
+    // dbgrp("not connected ... " + err.message)
+  }
+};
 
 // exports the hole database connection..
-// module.exports= DbConnection()          
+module.exports = DbConnection();
