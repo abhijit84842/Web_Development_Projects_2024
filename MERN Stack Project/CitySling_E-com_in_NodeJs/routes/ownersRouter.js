05ae8444706a737bbb4a58fbc5d2eb41663aa14b
Abhijit Das <abhijitd721201@gmail.com>
2024-07-19 17:33:07 +0530

    create owner ac and ui

--- MERN Stack Project/CitySling_E-com_in_NodeJs/routes/ownersRouter.js
+++ MERN Stack Project/CitySling_E-com_in_NodeJs/routes/ownersRouter.js
@@ -1,8 +1,7 @@
 const express = require("express");
 
 // model require
-const OwnerModel= require("../models/ownerModel");
-const ownerModel = require("../models/ownerModel");
+const OwnerModel = require("../models/ownerModel");
 
 const router = express.Router();
 
@@ -14,12 +13,14 @@ const router = express.Router();
 if (process.env.NODE_ENV === "development") {
   // this route only show in dev mode not production mode
   router.post("/create", async(req, res) => {
+    // console.log(req.body)
+
    let owner= await OwnerModel.find()
    if(owner.length > 0){
     return res.status(401).send("You don't have permission to create a new owner")
    }else{
-    let {fullname , email, password , gstin}=req.body
-    let result=await ownerModel.create({
+    let {fullname , email, password, gstin}=req.body
+    let result=await OwnerModel.create({
       fullname,
       email,
       password,
@@ -35,7 +36,7 @@ if (process.env.NODE_ENV === "development") {
 
 //* * Both Enviourment Base routing in middle...* *//
 router.get("/", (req, res) => {
-  res.send("hi ");
+  res.render("createowner");
 });
 
 //* *Only Production Enviourment Base routing in Buttom...* *//
