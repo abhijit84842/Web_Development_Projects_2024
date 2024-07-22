0a57129fe87842140a1ba92f5888561dec97c17e
Abhijit Das <abhijitd721201@gmail.com>
2024-07-22 11:22:50 +0530

    authentication done

--- MERN Stack Project/CitySling_E-com_in_NodeJs/routes/loginRouter.js
+++ MERN Stack Project/CitySling_E-com_in_NodeJs/routes/loginRouter.js
@@ -1,16 +1,31 @@
-const express = require('express')
+const express = require("express");
+const bcrypt = require("bcrypt");
 
-const router = express.Router()
+// Model require
+const UserModel = require("../models/userModel");
+const router = express.Router();
 
 // render the login page...
-router.get("/", (req,res)=>{
-    res.render("login")
-})
+router.get("/", (req, res) => {
+  res.render("login");
+});
 
 // post request
-router.post("/success" , (req,res)=>{
-    console.log(req.body)
-    res.redirect("/")
-})
+router.post("/success", async (req, res) => {
+  let { email, password } = req.body;
+  let user = await UserModel.findOne({ email: email });
+  if (!user) {
+    return res.status(404).send("incorrect email id...");
+  } else {
+    // compare with DB stored password...
+    bcrypt.compare(password, user.password, function (err, result) {
+      if (result==false) {
+       return res.send("incorrect password..")
+      } else {
+        res.send("login successfully..")
+      }
+    });
+  }
+});
 
-module.exports=router
+module.exports = router;
