7145b60897714bf05c2b068723bb705e83aba083
Abhijit Das <abhijitd721201@gmail.com>
2024-07-30 12:44:50 +0530

    user token update

--- MERN Stack Project/CitySling_E-com_in_NodeJs/middlewares/isLogin.js
+++ MERN Stack Project/CitySling_E-com_in_NodeJs/middlewares/isLogin.js
@@ -1,14 +1,17 @@
 const jwt= require("jsonwebtoken")
 
 const isLoggedIn =function(req,res,next){
-    // console.log(req.cookies.token)
-    if(req.cookies.token ===""){
+    // console.log(req.cookies.utoken)
+    if(req.cookies.utoken ===""){
         return res.redirect("/login")
     }
-    let cookiedata= jwt.verify(req.cookies.token , "secrect")
+    let cookiedata= jwt.verify(req.cookies.utoken , "secrect")
     // console.log(cookiedata)
     req.user1=cookiedata
     next()
 }
 
 module.exports = isLoggedIn
+
+
+/// USER LOGIN OR NOT CHECK MIDDLEWARE
