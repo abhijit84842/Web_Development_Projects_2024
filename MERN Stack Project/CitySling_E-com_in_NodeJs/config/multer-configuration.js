431dcaf02628075750f34a09185d0a42ac925db0
Abhijit Das <abhijitd721201@gmail.com>
2024-08-05 13:22:30 +0530

    install and configure multer

---
+++ MERN Stack Project/CitySling_E-com_in_NodeJs/config/multer-configuration.js
@@ -0,0 +1,24 @@
+//importing multer ES5
+const multer = require("multer");
+
+const crypto = require("crypto");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/images/upload"); // save the file destination
+  },
+  filename: function (req, file, cb) {
+    // use crypto to set unic file name
+    crypto.randomBytes(12, (err, bytes) => {
+      // console.log(bytes)            //get the buffer 12 digit buffer output
+      const filename = bytes.toString("hex") + path.extname(file.originalname); // convert in string and set the extention
+
+      cb(null, filename); // save the file name
+    });
+  },
+});
+
+const upload = multer({ storage: storage });
+
+module.exports=upload
