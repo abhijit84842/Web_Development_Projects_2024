27719c23348ef041488e37967309efb32425dbff
Abhijit Das <abhijitd721201@gmail.com>
2024-08-05 12:00:07 +0530

    save product details in db

--- MERN Stack Project/CitySling_E-com_in_NodeJs/routes/productsRouter.js
+++ MERN Stack Project/CitySling_E-com_in_NodeJs/routes/productsRouter.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+// middleware import
 const isUserLoggedIn= require("../middlewares/isLogin")
 
-const isAdminLoggedIn=require("../middlewares/adminIsLogIn")
+const isAdminLoggedIn=require("../middlewares/adminIsLogIn");
+
+// model import..
+const ProductModel= require("../models/productModel")
 
 // protected route..
 router.get("/", isUserLoggedIn, (req, res) => {
@@ -36,9 +40,27 @@ router.get("/addproducts", isAdminLoggedIn , (req,res)=>{
 })
 
 // POST req for add Products.
-router.post("/addproducts" , isAdminLoggedIn , (req,res)=>{
+router.post("/addproducts" , isAdminLoggedIn ,async (req,res)=>{
   console.log(req.body)
-  res.send("Product added successfully..")
+  let {image, name , subtitle, company, description,price,size,color,category,discount,features,material}=req.body
+  let products= await ProductModel.create({
+    image,
+    name,
+    subtitle,
+    company,
+    description,
+    price,
+    size,
+    color,
+    category,
+    discount,
+    features,
+    material,
+
+  })
+
+
+  res.send("Product added successfully.." + products)
 })
 
 
