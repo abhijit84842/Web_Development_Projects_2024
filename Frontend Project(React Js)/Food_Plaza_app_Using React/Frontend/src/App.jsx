55b35599b8fed9e1690851e5cee23f46d931b9a1
Abhijit Das <abhijitd721201@gmail.com>
2024-09-16 12:44:47 +0530

    create search box

--- Frontend Project(React Js)/Food_Plaza_app_Using React/Frontend/src/App.jsx
+++ Frontend Project(React Js)/Food_Plaza_app_Using React/Frontend/src/App.jsx
@@ -4,9 +4,15 @@ function App() {
     <div className={style.mainContainer}>
       <div className={style.topContainer}>
         <h1 className={style.h1Tag}>Welcome To Food Plaza</h1>
-        <logo className={style.logo}>
-          <img src="/Food Plaza.png" alt="loading.." />
-        </logo>
+        <div className={style.logoAndSearch}>
+          <logo className={style.logo}>
+            <img src="/Food Plaza.png" alt="loading.." />
+          </logo>
+          <div className={style.search}>
+            <input type="text" placeholder="Seacrch.." />
+          </div>
+        </div>
+
         <div className={style.subContainer}>
           <img src="/bg.png" alt="loadng..." />
         </div>
