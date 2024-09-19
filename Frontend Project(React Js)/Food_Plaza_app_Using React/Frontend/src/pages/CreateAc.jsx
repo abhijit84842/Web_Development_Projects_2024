1bf7e4b8939618c7cb513e0da58abfc9f1f4808c
Abhijit Das <abhijitd721201@gmail.com>
2024-09-19 12:58:17 +0530

    createAc form validation completed

--- Frontend Project(React Js)/Food_Plaza_app_Using React/Frontend/src/pages/CreateAc.jsx
+++ Frontend Project(React Js)/Food_Plaza_app_Using React/Frontend/src/pages/CreateAc.jsx
@@ -1,25 +1,74 @@
 import React from "react";
 import "../Css/createac.css";
+import { useForm } from "react-hook-form";
 
 const CreateAc = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
   return (
     <div className="acMainContainer">
       <h1>Create Your Ac</h1>
-      <form className="formTag" action="">
+      <form className="formTag" onSubmit={handleSubmit(onSubmit)}>
         <div className="inputFileds">
           <input
             type="text"
-            name=""
-            id="name"
+            {...register("name", {
+              required: "name is required",
+            })}
             placeholder="Enter Your name.."
           />
-          <input type="text" name="age" placeholder="Enter your age.." />
-          <input type="email" name="email" placeholder="Enter your email.." />
+          {errors.name && <p className="errorMsgForm">{errors.name.message}</p>}
           <input
             type="text"
-            name="password"
+            {...register("age", {
+              required: "age is required",
+            })}
+            placeholder="Enter your age.."
+          />
+          {errors.age && <p className="errorMsgForm">{errors.age.message}</p>}
+          <input
+            type="email"
+            {...register("email", {
+              required: "email is required",
+              pattern: {
+                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+                message: `invalid email..plz follow the pattern => [a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$`,
+              },
+              onChange: (e) => setValue("email", e.target.value.toLowerCase()),
+            })}
+            placeholder="Enter your email.."
+          />
+          {errors.email && (
+            <p className="errorMsgForm">{errors.email.message}</p>
+          )}
+
+          <input
+            type="text"
+            {...register("password", {
+              required: "password is required",
+              minLength: {
+                value: 6,
+                message: "password must be at least 6 characters long",
+              },
+              pattern: {
+                value:
+                  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+                message:
+                  "Password must contain at least one uppercase, one lowercase, one number, and one special character",
+              },
+            })}
             placeholder="Enter a new password"
           />
+          {errors.password && (
+            <p className="errorMsgForm">{errors.password.message}</p>
+          )}
           <button type="submit" className="submitButton">
             Submit
           </button>
