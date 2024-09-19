4957a10e1360e10b8ecde5ec0a9249d36dba2e58
Abhijit Das <abhijitd721201@gmail.com>
2024-09-19 12:19:47 +0530

    create ac ui design complete

--- Frontend Project(React Js)/Food_Plaza_app_Using React/Frontend/src/pages/CreateAc.jsx
+++ Frontend Project(React Js)/Food_Plaza_app_Using React/Frontend/src/pages/CreateAc.jsx
@@ -5,6 +5,26 @@ const CreateAc = () => {
   return (
     <div className="acMainContainer">
       <h1>Create Your Ac</h1>
+      <form className="formTag" action="">
+        <div className="inputFileds">
+          <input
+            type="text"
+            name=""
+            id="name"
+            placeholder="Enter Your name.."
+          />
+          <input type="text" name="age" placeholder="Enter your age.." />
+          <input type="email" name="email" placeholder="Enter your email.." />
+          <input
+            type="text"
+            name="password"
+            placeholder="Enter a new password"
+          />
+          <button type="submit" className="submitButton">
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
