6b7ca7ff0a41a0d331a6ee77b3b65b24259c6c2a
Abhijit Das <abhijitd721201@gmail.com>
2024-10-24 18:52:55 +0530

    create new socket.io server

--- MERN Stack Project/Chat App Using Web Socket/Backend/app.js
+++ MERN Stack Project/Chat App Using Web Socket/Backend/app.js
@@ -1,29 +1,45 @@
-const express = require("express")
-const path = require("path")
-const cors = require("cors")
+const express = require("express");
 
-const app = express()
+const path = require("path");
+const cors = require("cors");
 
-app.use(express.json())
 
-app.use(express.urlencoded({extended:true}))
+// import http module
+const http = require("http");
+//import socket.io server
+const { Server } = require("socket.io");
 
-app.use(express.static(path.join(__dirname,"public")))
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server)   // create new socket io server
+
+io.on("connection" , (socket)=>{        // here socket is basically client
+    console.log("A new user connected" , socket.id)
+})
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // handle cors origin error
-app.use(cors(
-    {
-        origin:"http://localhost:5173",
-        credentials: true // allow creadentials (cookies) to be sent with request
-    }
-))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // allow creadentials (cookies) to be sent with request
+  })
+);
 
-app.get("/api/name" ,(req,res)=>{
-    return res.json({
-        "name":"Abhijit Das",
-        "address":"Chandrakona Town"
-    })
-})
-app.listen(3000,()=>{
-    console.log("Server is Running PORT=> " + 3000)
-})
+app.get("/api/name", (req, res) => {
+  return res.json({
+    name: "Abhijit Das",
+    address: "Chandrakona Town",
+  });
+});
+server.listen(3000, () => {
+  console.log("Server is Running PORT=> " + 3000);
+});
