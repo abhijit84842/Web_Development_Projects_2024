f9eb9ac79950cf345233c5d658fa01d5d42b69a9
Abhijit Das <abhijitd721201@gmail.com>
2024-11-27 23:34:36 +0530

    use swiper js for slide show

--- Frontend Project(React Js)/Travel Agency Using React/src/App.jsx
+++ Frontend Project(React Js)/Travel Agency Using React/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import NavBar from "./Components/NavBar";
+import SwiperJsComp from "./Components/SwiperJsComp";
 
 function App() {
   return (
     <>
       <NavBar />
+      <SwiperJsComp />
     </>
   );
 }
---
+++ Frontend Project(React Js)/Travel Agency Using React/src/Components/SwiperJsComp.jsx
@@ -0,0 +1,170 @@
+// Import swiper React component
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "../CSS/swiperjscomp.css";
+
+// import required modules
+import { Parallax, Pagination, Navigation } from "swiper/modules";
+
+const SwiperJsComp = () => {
+  return (
+    <>
+      <Swiper
+        style={{
+          "--swiper-navigation-color": "#fff",
+          "--swiper-pagination-color": "#fff",
+        }}
+        speed={600}
+        parallax={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Parallax, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div
+            className="parallax-bg"
+            slot="container-start"
+            style={{
+              "background-image": "url(tokyo.jpg)",
+            }}
+            data-swiper-parallax="-23%"
+          ></div>
+          <div className="title" data-swiper-parallax="-300">
+            Slide 1
+          </div>
+          <div className="subtitle" data-swiper-parallax="-200">
+            Subtitle
+          </div>
+          <div className="text" data-swiper-parallax="-100">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
+              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
+              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
+              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
+              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="parallax-bg"
+            slot="container-start"
+            style={{
+              "background-image": "url(malaysia.jpg)",
+            }}
+            data-swiper-parallax="-23%"
+          ></div>
+          <div className="titel" data-swiper-parallax="-300">
+            Slide 2
+          </div>
+          <div className="subtitle" data-swiper-parallax="-200">
+            Subtile
+          </div>
+          <div className="text" data-swiper-parallax="-100">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
+              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
+              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
+              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
+              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="parallax-bg"
+            slot="container-start"
+            style={{
+              "background-image": "url(egypt.png)",
+            }}
+            data-swiper-parallax="-23%"
+          ></div>
+          <div className="titel" data-swiper-parallax="-300">
+            Slide 3
+          </div>
+          <div className="subtitle" data-swiper-parallax="-200">
+            Subtile
+          </div>
+          <div className="text" data-swiper-parallax="-100">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
+              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
+              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
+              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
+              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="parallax-bg"
+            slot="container-start"
+            style={{
+              "background-image": "url(thailand.jpg)",
+            }}
+            data-swiper-parallax="-23%"
+          ></div>
+          <div className="titel" data-swiper-parallax="-300">
+            Slide 4
+          </div>
+          <div className="subtitle" data-swiper-parallax="-200">
+            Subtile
+          </div>
+          <div className="text" data-swiper-parallax="-100">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
+              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
+              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
+              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
+              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+            </p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div
+            className="parallax-bg"
+            slot="container-start"
+            style={{
+              "background-image": "url(dubai.jpg)",
+            }}
+            data-swiper-parallax="-23%"
+          ></div>
+          <div className="titel" data-swiper-parallax="-300">
+            Slide 5
+          </div>
+          <div className="subtitle" data-swiper-parallax="-200">
+            Subtile
+          </div>
+          <div className="text" data-swiper-parallax="-100">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+              dictum mattis velit, sit amet faucibus felis iaculis nec. Nulla
+              laoreet justo vitae porttitor porttitor. Suspendisse in sem justo.
+              Integer laoreet magna nec elit suscipit, ac laoreet nibh euismod.
+              Aliquam hendrerit lorem at elit facilisis rutrum. Ut at
+              ullamcorper velit. Nulla ligula nisi, imperdiet ut lacinia nec,
+              tincidunt ut libero. Aenean feugiat non eros quis feugiat.
+            </p>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+
+export default SwiperJsComp;
