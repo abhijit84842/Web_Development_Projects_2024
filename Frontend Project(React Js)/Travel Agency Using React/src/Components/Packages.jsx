fbecd12d54a8df60a6b2be5b6b65750bbbb3fa75
Abhijit Das <abhijitd721201@gmail.com>
2024-11-28 13:11:09 +0530

    card design done

--- Frontend Project(React Js)/Travel Agency Using React/src/Components/Packages.jsx
+++ Frontend Project(React Js)/Travel Agency Using React/src/Components/Packages.jsx
@@ -4,6 +4,11 @@ import "../CSS/packages.css";
 // images import
 import mumbai from "../assets/images/mumbai.jpg";
 
+// React icons
+import { ImLocation } from "react-icons/im"; // location icon
+import { IoIosStar } from "react-icons/io"; // Star Rating icon
+import { IoStarOutline } from "react-icons/io5"; //  Star Rating icon
+
 const Packages = () => {
   return (
     <div className="package-container">
@@ -11,47 +16,252 @@ const Packages = () => {
       <div className="card-container">
         <div className="card">
           <img src={mumbai} alt="loading.." />
-          <div className="city-name">
-            <p>City Name</p>
-            <p className="subtitle">subtitle of city</p>
-          </div>
+          <div className="sub-card">
+            <div className="city-name">
+              <ImLocation style={{ color: "red" }} />
+              <p>Mumbai</p>
+            </div>
+            <div id="sub-card">
+              <p className="subtitle">
+                A Vibrant Tapestry of Culture, Ambition, and Endless
+                Possibilities
+              </p>
 
-          <div className="rating-box">
-            <div>*</div>
-            <div>*</div>
-            <div>*</div>
-            <div>*</div>
-          </div>
-          <div className="price-box">
-            <p>
-              $<span> 90</span>
-            </p>
-          </div>
-          <div className="button-box">
-            <button className="book-button">Book Now</button>
+              <div className="rating-box">
+                <div>
+                  <IoIosStar style={{ color: " red" }} size={25} />
+                </div>
+                <div>
+                  <IoIosStar style={{ color: "red" }} size={25} />
+                </div>
+                <div>
+                  <IoIosStar style={{ color: "red" }} size={25} />
+                </div>
+                <div>
+                  <IoIosStar style={{ color: "red" }} size={25} />
+                </div>
+                <div>
+                  <IoStarOutline size={25} />
+                </div>
+              </div>
+              <div className="price-box">
+                <p>
+                  $<span> 90</span>
+                </p>
+              </div>
+              <div className="button-box">
+                <button className="btn-grad ">Book Now</button>
+              </div>
+            </div>
           </div>
         </div>
 
         <div className="card">
           <img src={mumbai} alt="loading.." />
-          <div className="city-name">
-            <p>City Name</p>
-            <p className="subtitle">subtitle of city</p>
-          </div>
+          <div className="sub-card">
+            <div className="city-name">
+              <ImLocation style={{ color: "red" }} />
+              <p>Mumbai</p>
+            </div>
+            <div id="sub-card">
+              <p className="subtitle">
+                A Vibrant Tapestry of Culture, Ambition, and Endless
+                Possibilities
+              </p>
 
-          <div className="rating-box">
-            <div>*</div>
-            <div>*</div>
-            <div>*</div>
-            <div>*</div>
+              <div className="rating-box">
+                <div>
+                  <IoIosStar style={{ color: " red" }} size={25} />
+                </div>
+                <div>
+                  <IoIosStar style={{ color: "red" }} size={25} />
+                </div>
+                <div>
+                  <IoIosStar style={{ color: "red" }} size={25} />
+                </div>
+                <div>
+                  <IoIosStar style={{ color: "red" }} size={25} />
+                </div>
+                <div>
+                  <IoStarOutline size={25} />
+                </div>
+              </div>
+              <div className="price-box">
+                <p>
+                  $<span> 90</span>
+                </p>
+              </div>
+              <div className="button-box">
+                <button className="btn-grad ">Book Now</button>
+              </div>
+            </div>
           </div>
-          <div className="price-box">
-            <p>
-              $<span> 90</span>
-            </p>
+        </div>
+
+        <div className="card">
+          <img src={mumbai} alt="loading.." />
+          <div className="sub-card">
+            <div className="city-name">
+              <ImLocation style={{ color: "red" }} />
+              <p>Mumbai</p>
+            </div>
+            <div id="sub-card">
+              <p className="subtitle">
+                A Vibrant Tapestry of Culture, Ambition, and Endless
+                Possibilities
+              </p>
+
+              <div className="rating-box">
+                <div>
+                  <IoIosStar style={{ color: " red" }} size={25} />
+                </div>
+                <div>
+                  <IoIosStar style={{ color: "red" }} size={25} />
+                </div>
+                <div>
+                  <IoIosStar style={{ color: "red" }} size={25} />
+                </div>
+                <div>
+                  <IoIosStar style={{ color: "red" }} size={25} />
+                </div>
+                <div>
+                  <IoStarOutline size={25} />
+                </div>
+              </div>
+              <div className="price-box">
+                <p>
+                  $<span> 90</span>
+                </p>
+              </div>
+              <div className="button-box">
+                <button className="btn-grad ">Book Now</button>
+              </div>
+            </div>
           </div>
-          <div className="button-box">
-            <button className="book-button">Book Now</button>
+        </div>
+
+        <div className="card">
+          <img src={mumbai} alt="loading.." />
+          <div className="sub-card">
+            <div className="city-name">
+              <ImLocation style={{ color: "red" }} />
+              <p>Mumbai</p>
+            </div>
+            <div id="sub-card">
+              <p className="subtitle">
+                A Vibrant Tapestry of Culture, Ambition, and Endless
+                Possibilities
+              </p>
+
+              <div className="rating-box">
+                <div>
+                  <IoIosStar style={{ color: " red" }} size={25} />
+                </div>
+                <div>
+                  <IoIosStar style={{ color: "red" }} size={25} />
+                </div>
+                <div>
+                  <IoIosStar style={{ color: "red" }} size={25} />
+                </div>
+                <div>
+                  <IoIosStar style={{ color: "red" }} size={25} />
+                </div>
+                <div>
+                  <IoStarOutline size={25} />
+                </div>
+              </div>
+              <div className="price-box">
+                <p>
+                  $<span> 90</span>
+                </p>
+              </div>
+              <div className="button-box">
+                <button className="btn-grad ">Book Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="card">
+          <img src={mumbai} alt="loading.." />
+          <div className="sub-card">
+            <div className="city-name">
+              <ImLocation style={{ color: "red" }} />
+              <p>Mumbai</p>
+            </div>
+            <div id="sub-card">
+              <p className="subtitle">
+                A Vibrant Tapestry of Culture, Ambition, and Endless
+                Possibilities
+              </p>
+
+              <div className="rating-box">
+                <div>
+                  <IoIosStar style={{ color: " red" }} size={25} />
+                </div>
+                <div>
+                  <IoIosStar style={{ color: "red" }} size={25} />
+                </div>
+                <div>
+                  <IoIosStar style={{ color: "red" }} size={25} />
+                </div>
+                <div>
+                  <IoIosStar style={{ color: "red" }} size={25} />
+                </div>
+                <div>
+                  <IoStarOutline size={25} />
+                </div>
+              </div>
+              <div className="price-box">
+                <p>
+                  $<span> 90</span>
+                </p>
+              </div>
+              <div className="button-box">
+                <button className="btn-grad ">Book Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <img src={mumbai} alt="loading.." />
+          <div className="sub-card">
+            <div className="city-name">
+              <ImLocation style={{ color: "red" }} />
+              <p>Mumbai</p>
+            </div>
+            <div id="sub-card">
+              <p className="subtitle">
+                A Vibrant Tapestry of Culture, Ambition, and Endless
+                Possibilities
+              </p>
+
+              <div className="rating-box">
+                <div>
+                  <IoIosStar style={{ color: " red" }} size={25} />
+                </div>
+                <div>
+                  <IoIosStar style={{ color: "red" }} size={25} />
+                </div>
+                <div>
+                  <IoIosStar style={{ color: "red" }} size={25} />
+                </div>
+                <div>
+                  <IoIosStar style={{ color: "red" }} size={25} />
+                </div>
+                <div>
+                  <IoStarOutline size={25} />
+                </div>
+              </div>
+              <div className="price-box">
+                <p>
+                  $<span> 90</span>
+                </p>
+              </div>
+              <div className="button-box">
+                <button className="btn-grad ">Book Now</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
