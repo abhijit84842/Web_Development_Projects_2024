b6fb018989d0f7b642b0dd80f49c79b9e28cf70e
Abhijit Das <abhijitd721201@gmail.com>
2024-11-30 17:06:39 +0530

    apply form validation

--- Frontend Project(React Js)/Travel Agency Using React/src/Components/BookNow.jsx
+++ Frontend Project(React Js)/Travel Agency Using React/src/Components/BookNow.jsx
@@ -4,18 +4,18 @@ import React from "react";
 import worldtour from "../assets/images/worldtour.jpg";
 
 // // use form hooks
-// import { useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 import "../CSS/booknow.css";
 const BookNow = () => {
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   watch,
-  //   formState: { errors },
-  // } = useForm();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
-  // const onSubmit = data;
+  const onSubmit = (data) => console.log(data);
 
   return (
     <div className="booknow-main-container">
@@ -25,9 +25,46 @@ const BookNow = () => {
           <img src={worldtour} alt="loading..." />
         </div>
         <div className="right-container">
-          {/* <form onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" name="name" />
-          </form> */}
+          <h1>Book Your Tour</h1>
+          <form className="formTag" onSubmit={handleSubmit(onSubmit)}>
+            <div className="inputfiled">
+              <input
+                type="text"
+                {...register("where", {
+                  required: "this field is required..",
+                })}
+                placeholder="Write your current location.."
+              />
+              {errors.where && (
+                <p className="errorMsgForm">{errors.where.message}</p>
+              )}
+              <input
+                type="text"
+                {...register("to", {
+                  required: "this filed is required..",
+                })}
+                placeholder="Write your destination.."
+              />
+              {errors.to && <p className="errorMsgForm">{errors.to.message}</p>}
+              <input
+                type="number"
+                {...register("numOfPerson", {
+                  required: "this filed is required",
+                  min: {
+                    value: 1,
+                    message: "minimum 1 person is required..",
+                  },
+                })}
+                placeholder="Choose number of person.."
+              />
+              {errors.numOfPerson && (
+                <p className="errorMsgForm">{errors.numOfPerson.message}</p>
+              )}
+            </div>
+            <div className="submit-button">
+              <button type="submit">Book</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
