d22b1bfb4a878f2545d6f08f4ef658c80d96999e
Abhijit Das <abhijitd721201@gmail.com>
2024-12-02 12:38:25 +0530

    design star rating

--- Frontend Project(React Js)/Travel Agency Using React/src/Components/Review.jsx
+++ Frontend Project(React Js)/Travel Agency Using React/src/Components/Review.jsx
@@ -1,5 +1,10 @@
 import "../CSS/review.css";
 
+// react icons
+import { IoIosStar } from "react-icons/io"; // Star Rating icon
+import { IoStarOutline } from "react-icons/io5";
+import { IoIosStarHalf } from "react-icons/io";
+
 import abhijit from "../assets/images/reviewimages/abhijit.jpg";
 
 const Review = () => {
@@ -23,17 +28,33 @@ const Review = () => {
               future travels!
             </p>
             <div className="review-rating">
-              <div>*</div>
-              <div>*</div>
-              <div>*</div>
-              <div>*</div>
-              <div>*</div>
+              <div>
+                <IoIosStar style={{ color: "red" }} size={25} />
+              </div>
+              <div>
+                {" "}
+                <IoIosStar style={{ color: "red" }} size={25} />
+              </div>
+              <div>
+                {" "}
+                <IoIosStar style={{ color: "red" }} size={25} />
+              </div>
+              <div>
+                {" "}
+                <IoIosStar style={{ color: "red" }} size={25} />
+              </div>
+              <div>
+                {" "}
+                <IoIosStar style={{ color: "red" }} size={25} />
+              </div>
             </div>
           </div>
         </div>
-
         <div className="review-card">
-          <img src={abhijit} alt="loading.." />
+          <div className="customer-img-box">
+            <img src={abhijit} alt="loading.." />
+          </div>
+
           <div className="customer-details">
             <p className="customer-name">Abhijit Das</p>
             <p className="customer-review">
@@ -45,17 +66,34 @@ const Review = () => {
               future travels!
             </p>
             <div className="review-rating">
-              <div>*</div>
-              <div>*</div>
-              <div>*</div>
-              <div>*</div>
-              <div>*</div>
+              <div>
+                <IoIosStar style={{ color: "red" }} size={25} />
+              </div>
+              <div>
+                {" "}
+                <IoIosStar style={{ color: "red" }} size={25} />
+              </div>
+              <div>
+                {" "}
+                <IoIosStar style={{ color: "red" }} size={25} />
+              </div>
+              <div>
+                {" "}
+                <IoIosStar style={{ color: "red" }} size={25} />
+              </div>
+              <div>
+                {" "}
+                <IoStarOutline style={{ color: "red" }} size={25} />
+              </div>
             </div>
           </div>
         </div>
 
         <div className="review-card">
-          <img src={abhijit} alt="loading.." />
+          <div className="customer-img-box">
+            <img src={abhijit} alt="loading.." />
+          </div>
+
           <div className="customer-details">
             <p className="customer-name">Abhijit Das</p>
             <p className="customer-review">
@@ -67,11 +105,25 @@ const Review = () => {
               future travels!
             </p>
             <div className="review-rating">
-              <div>*</div>
-              <div>*</div>
-              <div>*</div>
-              <div>*</div>
-              <div>*</div>
+              <div>
+                <IoIosStar style={{ color: "red" }} size={25} />
+              </div>
+              <div>
+                {" "}
+                <IoIosStar style={{ color: "red" }} size={25} />
+              </div>
+              <div>
+                {" "}
+                <IoIosStar style={{ color: "red" }} size={25} />
+              </div>
+              <div>
+                {" "}
+                <IoIosStar style={{ color: "red" }} size={25} />
+              </div>
+              <div>
+                {" "}
+                <IoIosStarHalf style={{ color: "red" }} size={25} />
+              </div>
             </div>
           </div>
         </div>
