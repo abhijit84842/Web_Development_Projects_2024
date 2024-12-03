5c44408232ba45e86524554c3da1a48c404d35e5
Abhijit Das <abhijitd721201@gmail.com>
2024-12-03 20:10:37 +0530

    resize the container

--- Frontend Project(React Js)/Travel Agency Using React/src/Components/Domestic.jsx
+++ Frontend Project(React Js)/Travel Agency Using React/src/Components/Domestic.jsx
@@ -8,6 +8,7 @@ import { MdOutlineSportsVolleyball } from "react-icons/md";
 
 // images import
 import singapore from "../assets/images/domesticimages/singapore.jpg";
+import goa from "../assets/images/domesticimages/goa.jpg";
 
 // Import swiper react components
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -33,30 +34,81 @@ const Domestic = () => {
         className="swipercomp" // need unic classname to avoid css conflict
       >
         <SwiperSlide>
-          <img src={singapore} alt="loading.." />
-          <h2 className="domestic-name">Goa- The Special Beauty of Nature</h2>
-          <div className="activity">
-            <p>Beach, City Tours, Family, Honeymoon</p>
-          </div>
-          <div className="icons-activity">
-            <div className="place">
-              <FaEarthAmericas style={{ color: "blue" }} />
-              <p>1 Place</p>
+          <div className="slide-card">
+            <div className="img-box">
+              <img src={goa} alt="loading.." />
             </div>
-            <div className="activity-box">
-              <MdOutlineSportsVolleyball style={{ color: "red" }} />
-              <p>4 Activity</p>
+
+            <h2 className="domestic-name">Goa- The Special Beauty of Nature</h2>
+            <div className="activity">
+              <p>Beach, City Tours, Family, Honeymoon</p>
             </div>
-          </div>
-          <div className="blankdiv"></div>
-          <div className="city-desc">
-            The queen of beaches, Goa is the smallest city in India, but it
-            invites most of the attractive places in its vicinity. The
-            nightlife, alluring sand beaches, sacred sites and the world-class
-            heritage architecture of this place delivers the best…
+            <div className="icons-activity">
+              <div className="place">
+                <FaEarthAmericas style={{ color: "blue" }} />
+                <p>1 Place</p>
+              </div>
+              <div className="activity-box">
+                <MdOutlineSportsVolleyball style={{ color: "red" }} />
+                <p>4 Activity</p>
+              </div>
+            </div>
+            <div className="blankdiv"></div>
+            <div className="city-desc">
+              <p>
+                The queen of beaches, Goa is the smallest city in India, but it
+                invites most of the attractive places in its vicinity. The
+                nightlife, alluring sand beaches, sacred sites and the
+                world-class heritage architecture of this place delivers the
+                best…
+              </p>
+            </div>
+            <div className="blankdiv-2nd"></div>
+            <div className="tour-price">
+              <p>
+                ₹ <span>15,000.00</span>
+              </p>
+            </div>
           </div>
         </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>
+          <div className="slide-card">
+            <div className="img-box">
+              <img src={goa} alt="loading.." />
+            </div>
+
+            <h2 className="domestic-name">Rajasthan Tour 3N-4D</h2>
+            <div className="activity">
+              <p>Family, Safari</p>
+            </div>
+            <div className="icons-activity">
+              <div className="place">
+                <FaEarthAmericas style={{ color: "blue" }} />
+                <p>1 Place</p>
+              </div>
+              <div className="activity-box">
+                <MdOutlineSportsVolleyball style={{ color: "red" }} />
+                <p>2 Activity</p>
+              </div>
+            </div>
+            <div className="blankdiv"></div>
+            <div className="city-desc">
+              <p>
+                {" "}
+                Are you planning a quick getaway tour of one of the most
+                beautiful places in the middle of the Thar desert in Rajasthan,
+                the state known for its colourful festivals, forts, arts, music,
+                beautiful palaces
+              </p>
+            </div>
+            <div className="blankdiv-2nd"></div>
+            <div className="tour-price">
+              <p>
+                ₹ <span>15,000.00</span>
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
         <SwiperSlide>Slide 5</SwiperSlide>
