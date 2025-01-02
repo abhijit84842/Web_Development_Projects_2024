88281a592bee5ac9ba11055275b9fff845aed857
Abhijit Das <abhijitd721201@gmail.com>
2025-01-02 13:05:18 +0530

    user route set

--- MERN Stack Project/Minor_Project_practice/Frontend/minor_project_frontend/app/page.js
+++ MERN Stack Project/Minor_Project_practice/Frontend/minor_project_frontend/app/page.js
@@ -5,9 +5,9 @@ export default function Home() {
   return (
    <main>
     <h1 className="text-5xl flex justify-center mt-10">Minor Project</h1>
-    <div className="flex justify-center gap-20 mt-20">
+    <div className="flex justify-center gap-20 mt-20 ">
       <Link className="text-2xl" href={"/admin"}>Admin Panel</Link>
-      <Link className="text-2xl" href={"/userlogin"}>User Panel</Link>
+      <Link className="text-2xl" href={"/user"}>User Panel</Link>
    
     </div>
    </main>
--- MERN Stack Project/Minor_Project_practice/Frontend/minor_project_frontend/app/user/page.js
+++ MERN Stack Project/Minor_Project_practice/Frontend/minor_project_frontend/app/user/page.js
@@ -1,9 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-        <h1>Sign In Your Account</h1>
+        <h1 className='text-5xl flex justify-center mt-10 '>Sign In Your Account</h1>
+        <div className='flex justify-center gap-20 text-2xl mt-20'>
+          <Link href={"/user/createuserac"}>Create User Ac</Link>
+          <Link href={"/user/userlogin"}>User Login</Link>
+        </div>
+
     </div>
   )
 }
