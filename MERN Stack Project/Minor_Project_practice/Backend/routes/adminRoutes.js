73b14647c4f6edc04b25ffba3e77d910c8c6b94a
Abhijit Das <abhijitd721201@gmail.com>
2025-01-03 22:44:49 +0530

    multer setup for image

--- MERN Stack Project/Minor_Project_practice/Backend/routes/adminRoutes.js
+++ MERN Stack Project/Minor_Project_practice/Backend/routes/adminRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 
+// multer upload
+const upload = require("../config/multerConfiguration")
+
+
 const router = express.Router();
 
-router.post("/createac", (req, res) => {
-    console.log(req.body)
+router.post("/createac", upload.single("img"),(req, res) => {
+    let{name , age , email , password , phno}=req.body
+
     res.send("hi")
 
 });
