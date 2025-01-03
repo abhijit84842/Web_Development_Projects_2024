f7c2698e588831be4aec289be6e23fd27f64d5b6
Abhijit Das <abhijitd721201@gmail.com>
2025-01-04 00:08:11 +0530

    update readme file

--- MERN Stack Project/Minor_Project_practice/Backend/README.md
+++ MERN Stack Project/Minor_Project_practice/Backend/README.md
@@ -3,3 +3,7 @@
 <h2>MongoDb Atlast Admin Access </h2>
 - Username -> minorProjectPractice 
 - Password -> admin
+
+<h2>Website Admin Access </h2>
+email-abhijit721201@gmail.com
+password- abhijit@123
--- MERN Stack Project/Minor_Project_practice/Backend/config/Db.js
+++
@@ -1,2 +0,0 @@
-const dbStr="mongodb+srv://minorProjectPractice:admin@projectcluster1.zoxj2.mongodb.net/MinorProject_For_Practice?retryWrites=true&w=majority&appName=ProjectCluster1"
-module.exports= dbStr
