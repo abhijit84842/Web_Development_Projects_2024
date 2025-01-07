200237df9d5d22a81d8c3fb1753c127ac5dc76bb
Abhijit Das <abhijitd721201@gmail.com>
2025-01-07 17:29:22 +0530

    post req send from frontend with creadentials

--- MERN Stack Project/Minor_Project_practice/Frontend/minor_project_frontend/app/admin/adminlogin/page.js
+++ MERN Stack Project/Minor_Project_practice/Frontend/minor_project_frontend/app/admin/adminlogin/page.js
@@ -9,19 +9,35 @@ const page = () => {
     watch,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = async (data) => {
+    const res = await fetch("http://localhost:3001/admin/adminlogin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include", // include cookies
+      body: JSON.stringify(data),
+    });
+    let result = await res.json()
+    if(result.success){
+      alert(result.msg)
+    }else{
+      alert(result.msg)
+    }
   };
   return (
     <div>
       <h1 className="flex justify-center text-5xl mt-10">Admin Login</h1>
       <div className="sub-container flex justify-center mt-20">
-        <form className="flex justify-center flex-col gap-5 w-[25rem] bg-zinc-800 p-5" onSubmit={handleSubmit(onSubmit)}>
+        <form
+          className="flex justify-center flex-col gap-5 w-[25rem] bg-zinc-800 p-5"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <input
             className="p-2 rounded-md outline-none text-black"
             type="text"
             {...register("email", {
-              required:"email is required..."
+              required: "email is required...",
             })}
             placeholder="email id"
           />
@@ -29,12 +45,12 @@ const page = () => {
           <input
             className="p-2 rounded-md outline-none text-black"
             type="password"
-            {...register("password" , {
-              required:"password is required.."
+            {...register("password", {
+              required: "password is required..",
             })}
             placeholder="password"
           />
-           {errors.password && <p>{errors.password.message}</p>}
+          {errors.password && <p>{errors.password.message}</p>}
           <button className="bg-green-500 p-2 rounded-md" type="submit">
             Login
           </button>
