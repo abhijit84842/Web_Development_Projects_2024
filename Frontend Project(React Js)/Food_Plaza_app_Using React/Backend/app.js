dce36f7327e36b1918e039efd245867f60b62306
Abhijit Das <abhijitd721201@gmail.com>
2025-02-02 17:26:30 +0530

    code formating

--- Frontend Project(React Js)/Food_Plaza_app_Using React/Backend/app.js
+++ Frontend Project(React Js)/Food_Plaza_app_Using React/Backend/app.js
@@ -3,179 +3,182 @@ const path = require("path");
 
 const cors = require("cors");
 const { default: mongoose } = require("mongoose");
-const bcrypt = require("bcrypt")
-const JWT = require("jsonwebtoken")
-const cookieParser = require("cookie-parser")
+const bcrypt = require("bcrypt");
+const JWT = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 //Middleware import
-const IsLoggedIn = require("./middleware/isLoggedIn")
+const IsLoggedIn = require("./middleware/isLoggedIn");
 
-// multer 
-const upload = require("./config/multerConfig")
+// multer
+const upload = require("./config/multerConfig");
 
 // Model import
-const FoodModel = require("../Backend/models/foodModel")
-const OwnerModel = require("../Backend/models/ownerModel")
+const FoodModel = require("../Backend/models/foodModel");
+const OwnerModel = require("../Backend/models/ownerModel");
 
 const app = express();
 
-const URL=  "mongodb+srv://foodPlaza:admin@cluster0.l4vqt9f.mongodb.net/Food_Plaza_App_Using_React?retryWrites=true&w=majority&appName=Cluster0"
+const URL =
+  "mongodb+srv://admin:admin@cluster0.50snh.mongodb.net/Food_Plaza_App_Using_React?retryWrites=true&w=majority&appName=Cluster0";
 
 //json data
-app.use(express.json())
+app.use(express.json());
 
 // url data
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 
 // for static file
 app.use(express.static(path.join(__dirname, "public")));
 
 // Enable CORS for all routes
-app.use(cors({
-  origin: "http://localhost:5173",    // my frontend's origin
-  credentials:true    // Allow credentials (cookies) to be sent with requests
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173", // my frontend's origin
+    credentials: true, // Allow credentials (cookies) to be sent with requests
+  })
+);
 
 // Use cookie-parser to access and parse cookies in requests
-app.use(cookieParser())
-
+app.use(cookieParser());
 
 // Routing API
 
-app.get("/api/fooddata", async(req, res) => {
-
-  try{
-    await mongoose.connect(URL)
-    let data = await FoodModel.find()
-    if(!data){
-      res.json({mgs: "data not found.." , success: false})
-    }else{
-      res.json({data , success : true})
+app.get("/api/fooddata", async (req, res) => {
+  try {
+    await mongoose.connect(URL);
+    console.log("DB Connected successfully..");
+    let data = await FoodModel.find();
+    if (!data) {
+      res.json({ mgs: "data not found..", success: false });
+    } else {
+      res.json({ data, success: true });
     }
-  }catch(err){
-    console.log(err.message)
+  } catch (err) {
+    console.log(err.message);
   }
 });
 
-
 // POST API for create Owner AC
-app.post("/api/owner/createac" , async(req,res)=>{
+app.post("/api/owner/createac", async (req, res) => {
   // console.log(req.body)
 
-let {name , age , email , password } = req.body
-try{
-  await mongoose.connect(URL)
-  // console.log("connected")
-  let owner = await OwnerModel.find()
-  
-  if(owner.length > 0){
-    res.status(400).json({msg:"owner already exists" , success:false})
-  }
-  else{
-    bcrypt.genSalt(10,  function(err, salt) {
-      bcrypt.hash(password, salt, async function(err, hash) {
+  let { name, age, email, password } = req.body;
+  try {
+    await mongoose.connect(URL);
+    // console.log("connected")
+    let owner = await OwnerModel.find();
+
+    if (owner.length > 0) {
+      res.status(400).json({ msg: "owner already exists", success: false });
+    } else {
+      bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(password, salt, async function (err, hash) {
           // Store hash in your password DB.
-          try{
-            await mongoose.connect(URL)
+          try {
+            await mongoose.connect(URL);
             let result = await OwnerModel.create({
               name,
               age,
               email,
-              password:hash
-            })
+              password: hash,
+            });
 
-            res.status(200).json({ result, success:true})
-          }catch(err){
-            console.log(err.message)
+            res.status(200).json({ result, success: true });
+          } catch (err) {
+            console.log(err.message);
           }
+        });
       });
-  });
+    }
+  } catch (err) {
+    console.log(err.message);
   }
-
-
-}catch(err){
-  console.log(err.message)
-}
-
-  
-})
-
-
-// POST API FOR ADD FOODS
-app.post("/api/addfood", IsLoggedIn, upload.single("image") ,  async(req, res) => {
-  // console.log(req.file);
-  // console.log(req.body)
-  let {name , price , type , description} = req.body
-
-try{
-    await mongoose.connect(URL)
-    // console.log("DB connected successfully..")
-    let result = await FoodModel.create({
-      image:req.file.buffer,
-      name,
-      price,
-      type,
-      description
-
-    }) 
-    res.status(201).json({data: result ,msg:"Food added successfully.." , success: true})
-
-}catch(err){
-    console.log(err.message)
-}
 });
 
+// POST API FOR ADD FOODS
+app.post(
+  "/api/addfood",
+  IsLoggedIn,
+  upload.single("image"),
+  async (req, res) => {
+    // console.log(req.file);
+    // console.log(req.body)
+    let { name, price, type, description } = req.body;
+
+    try {
+      await mongoose.connect(URL);
+      // console.log("DB connected successfully..")
+      let result = await FoodModel.create({
+        image: req.file.buffer,
+        name,
+        price,
+        type,
+        description,
+      });
+      res
+        .status(201)
+        .json({
+          data: result,
+          msg: "Food added successfully..",
+          success: true,
+        });
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+);
 
 // POST login
-app.post("/api/login" ,async (req, res)=>{
-    // console.log(req.body)
-    let {email , password} = req.body
-    try{
-      await mongoose.connect(URL)
-      let owner = await OwnerModel.findOne({email})
-      if(!owner){
-        res.status(401).json({msg:"Owner does not exists.." , success:false})
-      }else{
-        // compare the password
-        bcrypt.compare(password, owner.password, function(err, result) {
-          // result == true
-          if(result == false){
-            res.status(401).json({msg:"incorrect password" , success: false})
-          }else{
-            // JWT Token set
-              let token =JWT.sign({email: owner.email , id: owner._id} ,"secrect" , {expiresIn: "4h"})
-              // console.log(token)
-              // set cookie
-              res.cookie("token" , token ,{
-                httpOnly:false ,   
-                // httpOnly:true,        // Ensures the cookie is only accessible by the server
+app.post("/api/login", async (req, res) => {
+  // console.log(req.body)
+  let { email, password } = req.body;
+  try {
+    await mongoose.connect(URL);
+    let owner = await OwnerModel.findOne({ email });
+    if (!owner) {
+      res.status(401).json({ msg: "Owner does not exists..", success: false });
+    } else {
+      // compare the password
+      bcrypt.compare(password, owner.password, function (err, result) {
+        // result == true
+        if (result == false) {
+          res.status(401).json({ msg: "incorrect password", success: false });
+        } else {
+          // JWT Token set
+          let token = JWT.sign(
+            { email: owner.email, id: owner._id },
+            "secrect",
+            { expiresIn: "4h" }
+          );
+          // console.log(token)
+          // set cookie
+          res.cookie("token", token, {
+            httpOnly: false,
+            // httpOnly:true,        // Ensures the cookie is only accessible by the server
 
-                secure:true,                 // Set to true if using HTTPS or set it false
-                
-                // sameSite:'Strict',             // Prevents CSRF attacks
-                sameSite:"none"                        // Allows the cookie to be sent in cross-site requests
+            secure: true, // Set to true if using HTTPS or set it false
 
-                // maxAge: 60 * 60 * 1000 // 1 hour
-              })
-              res.status(302).json({msg:"successfully login"  , success:true})
-          }
+            // sameSite:'Strict',             // Prevents CSRF attacks
+            sameSite: "none", // Allows the cookie to be sent in cross-site requests
+
+            // maxAge: 60 * 60 * 1000 // 1 hour
+          });
+          res.status(302).json({ msg: "successfully login", success: true });
+        }
       });
-      }
-    }catch(err){
-      console.log(err.message)
     }
-
-})
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 // logout Route
-app.get("/api/logout" , (req,res)=>{
+app.get("/api/logout", (req, res) => {
+  res.cookie("token", "");
+  res.json({ msg: "logout", success: true });
+});
 
-  res.cookie("token" ,"")
-  res.json({msg:"logout" , success:true})
-})
-
-
-
-app.listen(3000, () => {
-  console.log(`Port number is => 3000`);
+app.listen(3002, () => {
+  console.log(`Port number is => 3002`);
 });
