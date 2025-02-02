bf6dfa4a7255706ad0090a21b5234255e7b1bc8c
Abhijit Das <abhijitd721201@gmail.com>
2025-02-02 22:06:20 +0530

    update ui

--- Frontend Project(React Js)/Food_Plaza_app_Using React/Frontend/src/pages/AdminLoginPage.jsx
+++ Frontend Project(React Js)/Food_Plaza_app_Using React/Frontend/src/pages/AdminLoginPage.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import "../Css/login.css";
 
 // import useForm hook
 import { useForm } from "react-hook-form";
 
 const AdminLoginPage = () => {
+  const [shake, setShake] = useState(false);
   const {
     register,
     handleSubmit,
@@ -14,6 +16,7 @@ const AdminLoginPage = () => {
 
   // to get the form data
   const onSubmit = async (data) => {
+    setShake(true);
     // console.log(data);
     try {
       let res = await fetch("http://localhost:3002/api/login", {
@@ -37,38 +40,48 @@ const AdminLoginPage = () => {
     } catch (err) {
       alert("Error submiting form");
     }
+    setTimeout(() => setShake(false), 500);
   };
 
   return (
-    <div className="loginMainContainer">
-      <h1>Login Your Account</h1>
-      <div className="loginContainer">
-        <form className="subContainer" onSubmit={handleSubmit(onSubmit)}>
-          <input /* Email section validation  */
-            type="text"
-            {...register("email", {
-              required: "Email is required...",
-              pattern: {
-                // use email pattert type
-                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-                message: "Invalid email address",
-              },
-              onChange: (e) => setValue("email", e.target.value.toLowerCase()), // convert the email in lower case
-            })}
-            placeholder="Enter your email.."
-          />
-          {errors.email && (
-            <p className="errorMsgForm">{errors.email.message}</p>
-          )}
-          <input /* Password section validation  */
-            type="text"
-            {...register("password", { required: "Password is required.." })}
-            placeholder="Enter your password"
-          />
-          {errors.password && (
-            <p className="errorMsgForm">{errors.password.message}</p>
-          )}
-          <button className="loginButton" type="submit">
+    <div className="login-main-container ">
+      <div className={`login-sub-container ${shake ? "shake" : ""}`}>
+        <h2>Admin Login</h2>
+        <form className="admin-form" onSubmit={handleSubmit(onSubmit)}>
+          <div className="input-group">
+            <label>Email</label>
+            <input /* Email section validation  */
+              type="text"
+              {...register("email", {
+                required: "Email is required...",
+                pattern: {
+                  // use email pattert type
+                  value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+                  message: "Invalid email address",
+                },
+                onChange: (e) =>
+                  setValue("email", e.target.value.toLowerCase()), // convert the email in lower case
+              })}
+              placeholder="Enter your email.."
+              className="am-input"
+            />
+            {errors.email && (
+              <p className="errorMsgForm">{errors.email.message}</p>
+            )}
+          </div>
+          <div className="input-group">
+            <label>Password</label>
+            <input /* Password section validation  */
+              type="password"
+              {...register("password", { required: "Password is required.." })}
+              placeholder="Enter your password"
+              className="am-input"
+            />
+            {errors.password && (
+              <p className="errorMsgForm">{errors.password.message}</p>
+            )}
+          </div>
+          <button type="submit" className="am-button">
             Login
           </button>
         </form>
